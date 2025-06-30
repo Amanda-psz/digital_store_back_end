61bfdda18955dad20c61333bf98bda536b73db8a
Amanda Prudêncio <81720804+Amanda-psz@users.noreply.github.com>
2025-06-30 19:40:08 -0300

    atualizando database.js

--- src/config/database.js
+++ src/config/database.js
@@ -1,33 +1,18 @@
-// require('dotenv').config();
+ require('dotenv').config();
 
-// module.exports = {
-//   dialect: process.env.DB_DIALECT || 'postgres',
-//   host: process.env.DB_HOST,
-//   username: process.env.DB_USER,
-//   password: process.env.DB_PASSWORD,
-//   database: process.env.DB_NAME,
+ module.exports = {
+   dialect: process.env.DB_DIALECT || 'postgres',
+   host: process.env.DB_HOST,
+   username: process.env.DB_USER,
+   password: process.env.DB_PASSWORD,
+   database: process.env.DB_NAME,
   
-//   // Adicione esta linha para uma saída de teste mais limpa
-//   logging: false, 
+   // Adicione esta linha para uma saída de teste mais limpa
+   logging: false, 
   
-//   define: {
-//     timestamps: true,
-//     underscored: true,
-//     underscoredAll: true,
-//   },
-// };
-
-require('dotenv').config();
-
-module.exports = {
-  dialect: 'postgres',
-  host: process.env.DB_HOST,
-  username: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: process.env.DB_NAME,
-  port: process.env.DB_PORT || 5432,
-  define: {
-    timestamps: false,
-    underscored: true,
-  },
-};
+   define: {
+     timestamps: true,
+     underscored: true,
+     underscoredAll: true,
+   },
+ };
